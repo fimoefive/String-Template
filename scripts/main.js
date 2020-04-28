b4cef6eae1659d7cb4d0d763d5b278bd8de4e436
martin sisk <martin.sisk@pop.belmont.edu>
2020-04-28 11:38:13 -0500

    inital commit

---
+++ scripts/main.js
@@ -0,0 +1,11 @@
+// exercise 2
+
+const age = 30
+const timeSpan = 25
+const futureAge = age + timeSpan
+const name = "Biff"
+const generationStereotype = "millenial"
+const dadJoke = "milleni-old"
+
+console.log(`"Hi. My name is ${name}, and I'm ${age} years old. They say that makes me a ${generationStereotype}. 
+In ${timeSpan} years I'll be 55! I guess that means I'll be a ${dadJoke} then. Ha ha!"`);
